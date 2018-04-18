b4cc9724879616b19950d1278e6903c7d2c0d3f0
Blake Hoelle <bhoelle216@gmail.com>
2018-04-17 20:30:49 -0400

    commit

---
+++ JobSearchPlus/JobSearchPlus/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace JobSearchPlus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTransactionLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Define the connection string. See ConnectionStrings.com for reference.
+                string connectionString = @"Server=PL11\MTCDEVDB;Database=JobSearchPlus;Trusted_Connection=True;";
+
+                // Define the connection using the connection string
+                SqlConnection sqlConnecton = new SqlConnection(connectionString);
+
+                // Define a data adapter to pull the data from the server using the connection and a stored procedure
+                SqlDataAdapter dataAdapter = new SqlDataAdapter("dbo.ActiveContactList", sqlConnecton);
+
+                // Declare an empty data table to hold the data
+                DataTable DataTableGridView1 = new DataTable();
+
+                // Fill the data table using the data adapter
+                dataAdapter.Fill(DataTableGridView1);
+
+                // Use the data table as the data source for a data grid control
+                dataGridView1.DataSource = DataTableGridView1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                
+            }
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
