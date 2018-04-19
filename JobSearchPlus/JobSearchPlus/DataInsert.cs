46eba4fad99e59e8691ec51273f1f8a16b102340
Blake Hoelle <bhoelle216@gmail.com>
2018-04-18 21:25:33 -0400

    Tonights work

---
+++ JobSearchPlus/JobSearchPlus/DataInsert.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+// MUST HAVE using System.Data; using System.Data.SqlClient; ^^^^ when doing this
+
+namespace JobSearchPlus
+{
+    class DataInsert
+    {
+        // Declare a constant to hold the connection @"Server=;Database=;Trusted_Connection= (Windows Authentication Y or N)"
+        const string varConDefault = @"Server=PL11\MTCDEVDB;Database=JobSearchPlus;Trusted_Connection=True;";
+        // Declare SQL connection
+        SqlConnection varSqlconnect;
+
+        private bool DBConnect(string varConnectionString = "")
+        {
+            bool returnValue;
+
+            // If no connection string was specified, use the default
+            if (varConnectionString.Length == 0)
+            {
+                varConnectionString = varConDefault;
+            }
+
+            try
+            {
+                // Open the connection to SQL server
+                varSqlconnect = new SqlConnection(varConnectionString);
+                varSqlconnect.Open();
+                returnValue = true;
+            }
+            catch (Exception Ex)
+            {
+                returnValue = false;
+                throw Ex;
+            }
+
+            return returnValue; 
+        }
+
+        public bool InsertNewLead(DateTime RecordDate, string JobTitle, string Description, string EmploymentType
+            , string Location, out int LeadID)
+        {
+            LeadID = 0;
+            bool returnValue = false;
+
+            // Insert the specified information as a new job lead
+            try
+            {
+                if (DBConnect())
+                {
+                    //Create new command for stored procedure
+                    SqlCommand insertIntoLeads = new SqlCommand("InsertLeads", varSqlconnect);
+                    insertIntoLeads.CommandType = CommandType.StoredProcedure;
+
+                    //Link the Paramaters with the column names from Sql
+                    insertIntoLeads.Parameters.AddWithValue("@RecordDate", RecordDate);
+                    insertIntoLeads.Parameters.AddWithValue("@JobTitle", JobTitle);
+                    insertIntoLeads.Parameters.AddWithValue("@Description", Description);
+                    insertIntoLeads.Parameters.AddWithValue("@EmploymentType", EmploymentType);
+                    insertIntoLeads.Parameters.AddWithValue("@Location", Location);
+
+                    //Output for LeadID
+                    insertIntoLeads.Parameters.AddWithValue("@LeadID", LeadID).Direction = ParameterDirection.Output;
+
+                    // Run command
+                    insertIntoLeads.ExecuteNonQuery();
+
+                    // Retieve LeadID Value
+                    LeadID = int.Parse(insertIntoLeads.Parameters["@LeadID"].Value.ToString());
+
+                    returnValue = true;
+                }
+                
+            }
+            catch (Exception Ex)
+            {
+                returnValue = false;
+                throw Ex;
+            }
+
+            return returnValue;
+        }
+
+        public DataTable GetCallList()
+        {
+            // Define Datatable
+            DataTable dtCallList = new DataTable();
+
+            // Get data from jobsearch plus
+
+            try
+            {
+                if (DBConnect())
+                {
+                    // Define Adapter, "dbo.ActiveContactList" is stored procedure
+                    SqlDataAdapter sqlDA = new SqlDataAdapter("dbo.ActiveContactList", varSqlconnect);
+                    // Fill DataTable with storedProcedure results
+                    sqlDA.Fill(dtCallList);
+                }
+                else
+                {
+                    throw new Exception("Could not connext to JobSearch Plus database");
+                }
+                
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+
+            return dtCallList;
+        }
+
+        public DataTable GetLeadsBySource()
+        {
+            // Define DataTable
+            DataTable dtLeadsBySource = new DataTable();
+
+            try
+            {
+                if (DBConnect())
+                {
+                    // Define Adapter, "dbo.ActiveContactList" is stored procedure
+                    SqlDataAdapter sqlDA = new SqlDataAdapter("dbo.LeadsbySource", varSqlconnect);
+                    // Fill DataTable with storedProcedure results
+                    sqlDA.Fill(dtLeadsBySource);
+                }
+                else
+                {
+                    throw new Exception("Could not connext to JobSearch Plus database");
+                }
+
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+
+            return dtLeadsBySource;
+        }
+
+    }
+}
--- JobSearchPlus/JobSearchPlus/Form1.Designer.cs
+++ JobSearchPlus/JobSearchPlus/Form1.Designer.cs
@@ -29,33 +29,25 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.BtnTransactionLoad = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.button2 = new System.Windows.Forms.Button();
-            this.dataGridView3 = new System.Windows.Forms.DataGridView();
-            this.button3 = new System.Windows.Forms.Button();
-            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.ActiveCallList = new System.Windows.Forms.TabPage();
+            this.LeadsBySource = new System.Windows.Forms.TabPage();
             this.dataGridView4 = new System.Windows.Forms.DataGridView();
-            this.button4 = new System.Windows.Forms.Button();
-            this.tabPage5 = new System.Windows.Forms.TabPage();
-            this.dataGridView5 = new System.Windows.Forms.DataGridView();
-            this.button5 = new System.Windows.Forms.Button();
+            this.SearchLog = new System.Windows.Forms.TabPage();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.tbLocation = new System.Windows.Forms.TextBox();
+            this.tbEmploymentType = new System.Windows.Forms.TextBox();
+            this.tbDescription = new System.Windows.Forms.TextBox();
+            this.btnAddLead = new System.Windows.Forms.Button();
+            this.tbJobTitle = new System.Windows.Forms.TextBox();
+            this.tbRecordDate = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
-            this.tabPage3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
-            this.tabPage4.SuspendLayout();
+            this.ActiveCallList.SuspendLayout();
+            this.LeadsBySource.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
-            this.tabPage5.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
+            this.SearchLog.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -63,18 +55,8 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(6, 6);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(543, 291);
+            this.dataGridView1.Size = new System.Drawing.Size(543, 331);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // BtnTransactionLoad
-            // 
-            this.BtnTransactionLoad.Location = new System.Drawing.Point(458, 308);
-            this.BtnTransactionLoad.Name = "BtnTransactionLoad";
-            this.BtnTransactionLoad.Size = new System.Drawing.Size(75, 23);
-            this.BtnTransactionLoad.TabIndex = 1;
-            this.BtnTransactionLoad.Text = "Click Me";
-            this.BtnTransactionLoad.UseVisualStyleBackColor = true;
-            this.BtnTransactionLoad.Click += new System.EventHandler(this.BtnTransactionLoad_Click);
             // 
             // button1
             // 
@@ -88,146 +70,113 @@
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Controls.Add(this.tabPage3);
-            this.tabControl1.Controls.Add(this.tabPage4);
-            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.ActiveCallList);
+            this.tabControl1.Controls.Add(this.LeadsBySource);
+            this.tabControl1.Controls.Add(this.SearchLog);
             this.tabControl1.Location = new System.Drawing.Point(38, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(560, 369);
             this.tabControl1.TabIndex = 3;
             // 
-            // tabPage1
+            // ActiveCallList
             // 
-            this.tabPage1.BackColor = System.Drawing.Color.White;
-            this.tabPage1.Controls.Add(this.dataGridView1);
-            this.tabPage1.Controls.Add(this.BtnTransactionLoad);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(552, 343);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Active Call List";
+            this.ActiveCallList.BackColor = System.Drawing.Color.White;
+            this.ActiveCallList.Controls.Add(this.dataGridView1);
+            this.ActiveCallList.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.ActiveCallList.Location = new System.Drawing.Point(4, 22);
+            this.ActiveCallList.Name = "ActiveCallList";
+            this.ActiveCallList.Padding = new System.Windows.Forms.Padding(3);
+            this.ActiveCallList.Size = new System.Drawing.Size(552, 343);
+            this.ActiveCallList.TabIndex = 0;
+            this.ActiveCallList.Text = "Active Call List";
             // 
-            // tabPage2
+            // LeadsBySource
             // 
-            this.tabPage2.Controls.Add(this.button2);
-            this.tabPage2.Controls.Add(this.dataGridView2);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(552, 343);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "CallListForActiveLeads";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // tabPage3
-            // 
-            this.tabPage3.Controls.Add(this.button3);
-            this.tabPage3.Controls.Add(this.dataGridView3);
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(552, 343);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Lead Report";
-            this.tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView2
-            // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(3, 6);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(543, 291);
-            this.dataGridView2.TabIndex = 1;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(471, 303);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Click Me";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // dataGridView3
-            // 
-            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView3.Location = new System.Drawing.Point(6, 6);
-            this.dataGridView3.Name = "dataGridView3";
-            this.dataGridView3.Size = new System.Drawing.Size(543, 291);
-            this.dataGridView3.TabIndex = 2;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(440, 304);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Click Me";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // tabPage4
-            // 
-            this.tabPage4.Controls.Add(this.button4);
-            this.tabPage4.Controls.Add(this.dataGridView4);
-            this.tabPage4.Location = new System.Drawing.Point(4, 22);
-            this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(552, 343);
-            this.tabPage4.TabIndex = 3;
-            this.tabPage4.Text = "Leads By Source";
-            this.tabPage4.UseVisualStyleBackColor = true;
+            this.LeadsBySource.Controls.Add(this.dataGridView4);
+            this.LeadsBySource.Location = new System.Drawing.Point(4, 22);
+            this.LeadsBySource.Name = "LeadsBySource";
+            this.LeadsBySource.Size = new System.Drawing.Size(552, 343);
+            this.LeadsBySource.TabIndex = 3;
+            this.LeadsBySource.Text = "Leads By Source";
+            this.LeadsBySource.UseVisualStyleBackColor = true;
             // 
             // dataGridView4
             // 
             this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView4.Location = new System.Drawing.Point(3, 3);
             this.dataGridView4.Name = "dataGridView4";
-            this.dataGridView4.Size = new System.Drawing.Size(543, 291);
+            this.dataGridView4.Size = new System.Drawing.Size(543, 337);
             this.dataGridView4.TabIndex = 3;
             // 
-            // button4
+            // SearchLog
             // 
-            this.button4.Location = new System.Drawing.Point(471, 300);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "Click Me";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.SearchLog.Controls.Add(this.lblOutput);
+            this.SearchLog.Controls.Add(this.tbLocation);
+            this.SearchLog.Controls.Add(this.tbEmploymentType);
+            this.SearchLog.Controls.Add(this.tbDescription);
+            this.SearchLog.Controls.Add(this.btnAddLead);
+            this.SearchLog.Controls.Add(this.tbJobTitle);
+            this.SearchLog.Controls.Add(this.tbRecordDate);
+            this.SearchLog.Location = new System.Drawing.Point(4, 22);
+            this.SearchLog.Name = "SearchLog";
+            this.SearchLog.Size = new System.Drawing.Size(552, 343);
+            this.SearchLog.TabIndex = 4;
+            this.SearchLog.Text = "Search Log";
+            this.SearchLog.UseVisualStyleBackColor = true;
             // 
-            // tabPage5
+            // lblOutput
             // 
-            this.tabPage5.Controls.Add(this.button5);
-            this.tabPage5.Controls.Add(this.dataGridView5);
-            this.tabPage5.Location = new System.Drawing.Point(4, 22);
-            this.tabPage5.Name = "tabPage5";
-            this.tabPage5.Size = new System.Drawing.Size(552, 343);
-            this.tabPage5.TabIndex = 4;
-            this.tabPage5.Text = "Leads Need Activity";
-            this.tabPage5.UseVisualStyleBackColor = true;
+            this.lblOutput.BackColor = System.Drawing.Color.Wheat;
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOutput.Location = new System.Drawing.Point(15, 35);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(100, 23);
+            this.lblOutput.TabIndex = 8;
             // 
-            // dataGridView5
+            // tbLocation
             // 
-            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView5.Location = new System.Drawing.Point(6, 3);
-            this.dataGridView5.Name = "dataGridView5";
-            this.dataGridView5.Size = new System.Drawing.Size(543, 291);
-            this.dataGridView5.TabIndex = 4;
+            this.tbLocation.Location = new System.Drawing.Point(15, 179);
+            this.tbLocation.Name = "tbLocation";
+            this.tbLocation.Size = new System.Drawing.Size(100, 20);
+            this.tbLocation.TabIndex = 7;
             // 
-            // button5
+            // tbEmploymentType
             // 
-            this.button5.Location = new System.Drawing.Point(469, 301);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "Click Me";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.tbEmploymentType.Location = new System.Drawing.Point(15, 153);
+            this.tbEmploymentType.Name = "tbEmploymentType";
+            this.tbEmploymentType.Size = new System.Drawing.Size(100, 20);
+            this.tbEmploymentType.TabIndex = 6;
+            // 
+            // tbDescription
+            // 
+            this.tbDescription.Location = new System.Drawing.Point(15, 127);
+            this.tbDescription.Name = "tbDescription";
+            this.tbDescription.Size = new System.Drawing.Size(100, 20);
+            this.tbDescription.TabIndex = 5;
+            // 
+            // btnAddLead
+            // 
+            this.btnAddLead.Location = new System.Drawing.Point(132, 177);
+            this.btnAddLead.Name = "btnAddLead";
+            this.btnAddLead.Size = new System.Drawing.Size(75, 23);
+            this.btnAddLead.TabIndex = 4;
+            this.btnAddLead.Text = "Click Me";
+            this.btnAddLead.UseVisualStyleBackColor = true;
+            // 
+            // tbJobTitle
+            // 
+            this.tbJobTitle.Location = new System.Drawing.Point(15, 101);
+            this.tbJobTitle.Name = "tbJobTitle";
+            this.tbJobTitle.Size = new System.Drawing.Size(100, 20);
+            this.tbJobTitle.TabIndex = 3;
+            // 
+            // tbRecordDate
+            // 
+            this.tbRecordDate.Location = new System.Drawing.Point(15, 74);
+            this.tbRecordDate.Name = "tbRecordDate";
+            this.tbRecordDate.Size = new System.Drawing.Size(100, 20);
+            this.tbRecordDate.TabIndex = 2;
             // 
             // Form1
             // 
@@ -238,17 +187,14 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage2.ResumeLayout(false);
-            this.tabPage3.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
-            this.tabPage4.ResumeLayout(false);
+            this.ActiveCallList.ResumeLayout(false);
+            this.LeadsBySource.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
-            this.tabPage5.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
+            this.SearchLog.ResumeLayout(false);
+            this.SearchLog.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -256,22 +202,19 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button BtnTransactionLoad;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.TabPage tabPage3;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.DataGridView dataGridView3;
-        private System.Windows.Forms.TabPage tabPage4;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TabPage ActiveCallList;
+        private System.Windows.Forms.TabPage LeadsBySource;
         private System.Windows.Forms.DataGridView dataGridView4;
-        private System.Windows.Forms.TabPage tabPage5;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.DataGridView dataGridView5;
+        private System.Windows.Forms.TabPage SearchLog;
+        private System.Windows.Forms.Button btnAddLead;
+        private System.Windows.Forms.TextBox tbJobTitle;
+        private System.Windows.Forms.TextBox tbRecordDate;
+        private System.Windows.Forms.TextBox tbLocation;
+        private System.Windows.Forms.TextBox tbEmploymentType;
+        private System.Windows.Forms.TextBox tbDescription;
+        private System.Windows.Forms.Label lblOutput;
     }
 }
 
